1ecfd6775bde914e5dd9ac378662c2609d23b7e7
Arthur Hovanesian <nameonascreen2@gmail.com>
2018-11-27 20:13:46 -0800

    Implement API call to retrieve data

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const axios = require('axios');
 const port = 3000;
 
 const app = express();
 
 //change this to path.join???
 app.use(express.static(__dirname + '/../public'));
+
+app.get('/api/choose_word', (req, res) => {
+  axios.get(`http://app.linkedin-reach.io/words`)
+    .then(success => res.status(200).send(chooseWord(success.data)))
+    .catch(err => res.status(500).send(err));
+})
+
+chooseWord = (data) => {
+  const words = data.split('\n');
+  const randInd = Math.floor(Math.random() * words.length);
+  return words[randInd];
+}
 
 app.listen(port, () => {
   console.log('Listening on port ' + port);
--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  //is props necessary?
+  constructor() {
+    super();
+    //spinner for loading word
     this.state = {
-
+      word: '',
     };
   };
 
+  componentWillMount() {
+    this.getWord();
+  }
+
+  getWord() {
+    //err handling
+    axios.get('/api/choose_word')
+      .then(res => this.setState({word: res.data}))
+      .catch(err => console.log(err));
+  }
+
   render() {
+    console.log(this.state.word)
     return (
-      <h1>hi</h1>
+      <h1>{this.state.word}</h1>
     )
   };
 };
 
+//export up at the top?
 export default App;
