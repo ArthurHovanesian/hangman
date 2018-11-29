d62d1ceada8f0230f227fa454392ade3053e533e
Arthur Hovanesian <nameonascreen2@gmail.com>
2018-11-29 11:32:20 -0800

    Display lives remaining for all 6 incorrect guesses

--- src/Lives/Lives.jsx
+++ src/Lives/Lives.jsx
@@ -10,22 +10,57 @@ class Lives extends React.Component {
   }
 
   render() {
-    return (
-      <div>
+    const { attemptsLeft } = this.props;
+    if (attemptsLeft === 6) {
+      return (
         <div className={styles.container}>
-          <svg height="300" className={`${styles.scale} styles.fade`}>
+          <svg height="300" className={`${styles.scale} ${styles.fade}`}>
             <polyline points="300,150 225,280 75,280 0,150 75,20 225,20 300,150 0,150 75,20 225,280 75,280 225,20" fill="white" stroke="black" stroke-width="2" fill-rule="nonzero"/>
           </svg>
         </div>
+      )
+    } else if (attemptsLeft === 5) {
+      return (
         <div className={styles.container}>
           <svg height="300" className={styles.scale}>
             <polyline points="300,150 225,280 75,280 0,150 75,20 225,280 75,280 225,20 300,150 0,150" fill="white" stroke="black" stroke-width="2" fill-rule="nonzero"/>
           </svg>
         </div>
-      </div>
-    )
+      )
+    } else if (attemptsLeft === 4) {
+      return (
+        <div className={styles.container}>
+          <svg height="300" className={styles.scale}>
+            <polyline points="150,150 300,150 225,280 150,150 75,280 225,280 150,150 0,150 75,280 150,150 75,20 0,150" fill="white" stroke="black" stroke-width="2" fill-rule="nonzero"/>
+          </svg>
+        </div>
+      )
+    } else if (attemptsLeft === 3) {
+      return (
+        <div className={styles.container}>
+          <svg height="300" className={styles.scale}>
+            <polyline points="150,150 225,280 75,280 150,150 0,150 75,280 150,150 75,20 0,150" fill="white" stroke="black" stroke-width="2" fill-rule="nonzero"/>
+          </svg>
+        </div>
+      )
+    } else if (attemptsLeft === 2) {
+      return (
+        <div className={styles.container}>
+          <svg height="300" className={styles.scale}>
+            <polyline points="75,280 0,150 75,20 150,150 75,280 0,150 150,150" fill="white" stroke="black" stroke-width="2" fill-rule="nonzero"/>
+          </svg>
+        </div>
+      )
+    } else if (attemptsLeft === 1) {
+      return (
+        <div className={styles.container}>
+          <svg height="300" className={styles.scale}>
+            <polyline points="0,150 75,20 150,150 0,150" fill="white" stroke="black" stroke-width="2" fill-rule="nonzero"/>
+          </svg>
+        </div>
+      )
+    }
   }
 }
-
 
 export default Lives;
