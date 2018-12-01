c45eee3fd7bf9ad10b04c8475594a72de59d7f5d
Arthur Hovanesian <nameonascreen2@gmail.com>
2018-11-30 23:25:09 -0800

    Refactor life indicator component to reduce repetition

--- src/Lives/Lives.jsx
+++ src/Lives/Lives.jsx
@@ -1,73 +1,40 @@
 import React from 'react';
 import styles from './Lives.css';
 
-class Lives extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const Lives = ({ attemptsLeft }) => {
 
-    }
+  const color = {
+    1: 'red',
+    2: '#FF4500',
+    3: 'yellow',
+    4: '#39FF14',
+    5: '#00FFFF',
+    6: '#FF0DBF'
   }
 
-  render() {
-    const { attemptsLeft } = this.props;
-    if (attemptsLeft === 6) {
-      return (
-        <div className={styles.container}>
-          <svg height="300" className={`${styles.scale} ${styles.fade}`}>
-            <polyline points="300,150 225,280 75,280 0,150 75,20 225,20 300,150 0,150 75,20 225,280 75,280 225,20" stroke="#ff0dbf" stroke-width="2" fill-rule="nonzero"/>
-          </svg>
-        </div>
-      )
-    } else if (attemptsLeft === 5) {
-      return (
-        <div className={styles.container}>
-          <svg height="300" className={styles.scale}>
-            <polyline points="300,150 225,280 75,280 0,150 75,20 225,280 75,280 225,20 300,150 0,150" stroke="#00FFFF" stroke-width="2" fill-rule="nonzero"/>
-          </svg>
-        </div>
-      )
-    } else if (attemptsLeft === 4) {
-      return (
-        <div className={styles.container}>
-          <svg height="300" className={styles.scale}>
-            <polyline points="150,150 300,150 225,280 150,150 75,280 225,280 150,150 0,150 75,280 150,150 75,20 0,150" stroke="#39FF14" stroke-width="2" fill-rule="nonzero"/>
-          </svg>
-        </div>
-      )
-    } else if (attemptsLeft === 3) {
-      return (
-        <div className={styles.container}>
-          <svg height="300" className={styles.scale}>
-            <polyline points="150,150 225,280 75,280 150,150 0,150 75,280 150,150 75,20 0,150" stroke="yellow" stroke-width="2" fill-rule="nonzero"/>
-          </svg>
-        </div>
-      )
-    } else if (attemptsLeft === 2) {
-      return (
-        <div className={styles.container}>
-          <svg height="300" className={styles.scale}>
-            <polyline points="75,280 0,150 75,20 150,150 75,280 0,150 150,150" stroke="#ff4500" stroke-width="2" fill-rule="nonzero"/>
-          </svg>
-        </div>
-      )
-    } else if (attemptsLeft === 1) {
-      return (
-        <div className={styles.container}>
-          <svg height="300" className={styles.scale}>
-            <polyline points="0,150 75,20 150,150 0,150" stroke="red" stroke-width="2" fill-rule="nonzero"/>
-          </svg>
-        </div>
-      )
-    } else if (attemptsLeft === 0) {
-      return (
-        <div className={styles.container}>
-          <svg height="300" className={styles.scale}>
-          </svg>
-        </div>
-      )
-    }
+  const polygon = {
+    1: '0,150 75,20 150,150 0,150',
+    2: '75,280 0,150 75,20 150,150 75,280 0,150 150,150',
+    3: '150,150 225,280 75,280 150,150 0,150 75,280 150,150 75,20 0,150',
+    4: '150,150 300,150 225,280 150,150 75,280 225,280 150,150 0,150 75,280 150,150 75,20 0,150',
+    5: '300,150 225,280 75,280 0,150 75,20 225,280 75,280 225,20 300,150 0,150',
+    6: '300,150 225,280 75,280 0,150 75,20 225,20 300,150 0,150 75,20 225,280 75,280 225,20'
   }
+
+  const lifeBar = (lives) => {
+    return (
+        <svg height="300" className={`${styles.scale}`} style={{filter: `drop-shadow(0 0 10px ${color[lives]})`}}>
+          <polyline points={polygon[lives]} stroke={color[lives]} stroke-width="2" fill-rule="nonzero"/>
+        </svg>
+    )
+  }
+
+  return (
+    <div className={styles.container}>
+      {lifeBar(attemptsLeft)}
+    </div>
+  )
+
 }
 
 export default Lives;
