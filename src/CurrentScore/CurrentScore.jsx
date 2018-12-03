53ece17af9d3c54b42cdc48d8533c461f35b5cbb
Arthur Hovanesian <nameonascreen2@gmail.com>
2018-12-03 00:41:03 -0800

    Add styling to score display

--- src/CurrentScore/CurrentScore.jsx
+++ src/CurrentScore/CurrentScore.jsx
@@ -5,15 +5,7 @@ import styles from './CurrentScore.css';
 const CurrentScore = ({ score, name, difficulty }) => {
   return (
     <div className={styles.currentscore}>
-      <div>
-        {name}
-      </div>
-      <div>
-        {difficulty}
-      </div>
-      <div>
-        {score}
-      </div>
+      {score}
     </div>
   )
 }
